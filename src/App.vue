cce54e3a383e9d98ce1a3972254bfdf24243996f
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-05-29 22:10:29 +0900

    updated description

--- src/App.vue
+++ src/App.vue
@@ -207,7 +207,7 @@
 
     <div class="pt-2">
       <div
-        class="flex pb-5 px-3 m-auto border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl"
+        class="flex justify-between items-start pb-5 m-auto border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl"
       >
         <div class="mt-2">
           Made with ❤ for
@@ -215,6 +215,17 @@
           <a href="https://github.com/Xatta-Trone/" target="_blank"
             >Xatta Trone</a
           >
+          <a class="inline-block mx-2" href="https://github.com/Xatta-Trone" target="_blank"
+            ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>
+          </a>
+          <a class="inline-block" href="https://www.linkedin.com/in/monzurul-islam/" target="_blank"
+            ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M8 11l0 5"></path><path d="M8 8l0 .01"></path><path d="M12 16l0 -5"></path><path d="M16 16v-3a2 2 0 0 0 -4 0"></path></svg>
+          </a>
+        </div>
+        <div class="mt-2">
+          <a href="https://docs.google.com/spreadsheets/d/1jRATLVV34vATsL4Y67fZZXQc7qZPYc0c0Yk7Bykh4fw/edit#gid=0" target="_blank">Original vocabulary list</a>
         </div>
       </div>
     </div>
@@ -408,8 +419,7 @@ export default {
     randomWord: function () {
       this.searchquery = "";
       this.lower_selected = 0;
-      this.current_word =
-        this.words[Math.floor(Math.random() * this.words.length)];
+      this.current_word = this.words[Math.floor(Math.random() * this.words.length)];
     },
     lowerChange() {
       console.log(this.lower_selected);
